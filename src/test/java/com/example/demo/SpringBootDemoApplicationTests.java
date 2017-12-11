0ece02f78549de511d7bbf1f2c3ba2801e09b8cf
wangnan <nan.wang@htouhui.com>
2017-12-11 14:53:56 +0800

    INIT

--- src/test/java/com/example/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootDemoApplicationTests.java
@@ -1,47 +1,15 @@
 package com.example.demo;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 public class SpringBootDemoApplicationTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+    public void loadContext() {
 
-    @Test
-    public void resolver() throws Exception {
-        String url = "/submit";
-        mockMvc.perform(post(url)
-                .accept(MediaType.MULTIPART_FORM_DATA)
-                .param("teacher.name", "teacher")
-                .param("student.name", "student")
-                .param("student.age", "21"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void test() throws Exception {
-        String url = "/test";
-        mockMvc.perform(post(url)
-                .accept(MediaType.MULTIPART_FORM_DATA)
-                .param("name", "student")
-                .param("age", "21"))
-                .andDo(print())
-                .andExpect(status().isOk());
     }
 
 }
